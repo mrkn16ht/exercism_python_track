6a0a409beecfa39cc80bb3e85314c21bb01d4271
mrkn16ht <46416323+mrkn16ht@users.noreply.github.com>
2019-03-21 20:57:56 +0800

    Add files via upload

---
+++ isogram.py
@@ -0,0 +1,13 @@
+#marked with comment(#) is the original value
+
+#def is_isogram(string):
+#    pass
+
+def is_isogram(string):
+    pass
+
+def is_isogram(string):
+    string = string.lower()
+    string = string.replace('-', '')
+    string = string.replace(' ', '')
+    return len(set(string)) == len(string)

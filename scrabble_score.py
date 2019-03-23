9e37ab221e86c904ce5188038b427854f7c25eb6
mrkn16ht <46416323+mrkn16ht@users.noreply.github.com>
2019-03-23 23:45:26 +0800

    Add files via upload

---
+++ scrabble_score.py
@@ -0,0 +1,20 @@
+#marked with comment(#) is the original value
+
+#def score(word):
+#    pass
+
+def score(word):
+    scores = {'A, E, I, O, U, L, N, R, S, T': 1,
+              'D, G': 2,
+              'B, C, M, P': 3,
+              'F, H, V, W, Y': 4,
+              'K': 5,
+              'J, X': 8,
+              'Q, Z': 10}
+
+    total = 0
+    for a in word.upper():
+        for b in scores:
+            if a in b:
+                total += scores[b]
+    return total
